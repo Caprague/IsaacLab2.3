171318e14293fc33a799719c85664692653e12d5
Caprague <2210134086@qq.com>
2026-08-06 03:52:52 +0800

    up

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/go2_loco_skill_walk_mid360_depth_10hz_gru_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/go2_loco_skill_walk_mid360_depth_10hz_gru_cfg.py
@@ -7,8 +7,9 @@
 ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py \
     --task Go2-Loco-Skill-Walk-Mid360Depth-10Hz-GRU \
     --agent rsl_rl_distillation_cfg_entry_point \
-    --checkpoint /home/ls_gms/Isaac/IsaacLab2.3/logs/rsl_rl/Go2-Loco-Skill-Walk-Mid360Depth-10Hz-GRU/{run}_pretrain_teacher/model_teacher.pt \
-    --headless --num_envs 2048
+    --load_run ".*_pretrain_teacher" \
+    --checkpoint "model_teacher" \
+    --headless --num_envs 4096
 """
 
 # ============================================================================================================
