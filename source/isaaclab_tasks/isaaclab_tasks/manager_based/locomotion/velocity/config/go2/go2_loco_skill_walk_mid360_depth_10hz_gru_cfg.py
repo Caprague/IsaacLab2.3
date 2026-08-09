b9e29585653b86d48a492c43041b0d4f1f88edec
Caprague <2210134086@qq.com>
2026-08-10 03:38:42 +0800

    update

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/go2_loco_skill_walk_mid360_depth_10hz_gru_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/go2/go2_loco_skill_walk_mid360_depth_10hz_gru_cfg.py
@@ -738,7 +738,7 @@ class CurriculumCfg:
 class Go2LocomotionSkillEnvCfg(ManagerBasedRLEnvCfg):
     """Configuration for the locomotion velocity-tracking environment."""
 
-    stage: str = "stage4"
+    stage: str = "stage1"
     """训练阶段: stage1(低速向前)/stage2(全向移动，基础模型)/stage3(全向移动，mid360模型)/stage4(student训练，启用mid360深度图)"""
 
     environment: str = "server"
